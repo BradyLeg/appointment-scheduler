686abbc51a9163a7b1c3bdfe9d7929ef65491b2c
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-01-30 14:44:32 -0800

    Served page using express

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ app.use(express.static('public'));
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/appointmentform.html`);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
